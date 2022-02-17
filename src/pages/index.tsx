9f5f48caf54361f223f915166319e799903a5918
AmauriLima <amauri.plimaj@gmail.com>
2022-02-16 21:46:24 -0300

    init: initializing project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+import { PageTitle } from '@components/PageLayout';
+
+const Home: NextPage = () => (
+  <>
+    <PageTitle>Youtube Ranking - Alternativa Software</PageTitle>
+    <h1>Oi</h1>
+  </>
+);
+
+export default Home;
