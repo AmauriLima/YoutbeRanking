6b6029f1ade3cba4db5ceea31df98bb35efb54ed
AmauriLima <amauri.plimaj@gmail.com>
2022-02-16 21:55:50 -0300

    feat: adding styled-components

---
+++ src/assets/styles/themes/default.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from 'styled-components';
+
+export default {
+  colors: {
+    white: '#ffffff',
+    gray: {
+      900: '#121214',
+    },
+  },
+} as DefaultTheme;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from '@assets/styles/global';
+import defaultTheme from '@assets/styles/themes/default';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
